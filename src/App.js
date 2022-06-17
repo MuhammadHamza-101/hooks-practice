adc5f130ca149f3434bdb985d912b11e33991597
Muhammad-Hamza <77409746+MuhammadHamza-101@users.noreply.github.com>
2022-06-17 09:45:39 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useReducer } from 'react';
 import './App.css';
+import Practice from './demo'
+
+import { ErrorBoundary } from "react-error-boundary";
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  )
+}
+
+//create reducer function
+
+// const reducer = (state, action) => {
+
+//   switch (action.type) {
+//     case "Increment":
+//       return { count: state.count + 1 }
+//     // case "TextValue":
+//     //   return { text: state.text = "" }
+//     default:
+//       return state
+//   };
+// }
+
+
+
 
 function App() {
+
+  //  useState hook
+
+  // let [counter, setCounter] = useState(0)
+  // const [text, setText] = useState("type Hurry!")
+
+  //use reducer hook
+
+  // const [state, dispatch] = useReducer(reducer, { count: 0, text: "hamza" })
+
+  //functions 
+
+  // let Increment = () => {
+  //   counter = counter + 1
+  //   setCounter(counter)
+
+  // }
+
+  // let onChange = (e) => {
+  //   setText(e.target.value)
+  //   // console.log()
+  // }
+
+
+
+  //render component 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => {
+          // reset the state of your app so the error doesn't happen again
+        }}
+      >
+        <Practice />
+      </ErrorBoundary>
     </div>
   );
 }
---
+++ src/demo.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect, useReducer, useRef } from "react";
+import "./App.css";
+
+function Practice() {
+  return (
+    <div className="App">
+      <div>{}</div>
+
+      <div>
+        <input placeholder="write something "></input>
+        <button>change name</button>
+      </div>
+    </div>
+  );
+}
+
+export default Practice;
